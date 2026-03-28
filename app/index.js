8f89ad774645d5e37a68335c1b2db25853c0fc15
Nicolas Conan <nicolas.conan.88@gmail.com>
2026-03-28 22:03:29 +0100

    ok live

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
-import socketio from "socket.io";
+// import { Server } from "socket.io";
 // fichiers locaux :
 import routerBack from './back/router.js';
 import websocket from './back/websocket.js';
@@ -8,7 +8,7 @@ import websocket from './back/websocket.js';
 const app = express();
 
 // Socket.io :
-// const io = new socketio.Server(app);
+// const io = new Server(app);
 // io.on('connection', websocket);
 
 app.use('/api', routerBack);
