1120e9000b46aa3c070e32fa67b1aa7523141f99
TheVeiler <duboisvalentin1010@gmail.com>
2026-03-31 17:00:54 +0200

    setup architecture

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # pili
+
+## Architecture
+
+```py
+app/
+    front/
+        dist/        # front buildé
+        src/         # fichiers source du front
+        index.html   # point d'entrée principal du front
+    back/
+        src/         # fichiers source du back
+        index.js     # point d'entrée principal du back
+```
+
+## Techs
+
+- Front : React buildé avec Vite
+- Back : Node + Express
+- Lang : JS
+- API : Websockets & JSON
+
+## Scènes
+
+- sso
+- lobby
+- game
+- game_results
---
+++ app/back/index.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import http from "node:http";
+import { Server } from "socket.io";
+
+import routerBack from "./src/router.js";
+import websocket from "./src/websocket.js";
+
+const app = express();
+const port = process.env.SERVER_PORT;
+const server = http.createServer(app);
+
+// Socket.io :
+const io = new Server(server, {
+	cors: {
+		origin: "*",
+	},
+});
+io.on("connection", websocket);
+
+app.use(express.json());
+app.use(express.static("app/public"));
+app.use(cors());
+
+app.use("/api", routerBack);
+app.use((req, res) => {
+	if (process.env.NODE_ENV === "production") {
+		//! HTTP_301 n'est pas valide car le serveur sert normalement une page html. Ce n'est pas une redirection permanente d'une ressource qui existait à une précédente adresse vers une nouvelle adresse.
+		//? Je laisse pour le moment, au cas où Ultra en a besoin.
+		res.status(301).sendFile("index.html", { root: "../front/dist" });
+	} else {
+		res.set("Content-Type", "text/plain").send("Server opérationnel");
+	}
+});
+
+server.listen(port, () => {
+	console.log("backend disponible à l'adresse : http://localhost:" + port);
+});
